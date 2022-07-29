1105fbfeb64aa8a76950020ceac24b0cb8329fde
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2022-07-29 16:03:03 +0300

    get adjacency matrix

---
+++ utils.py
@@ -0,0 +1,16 @@
+def get_adjacency_matrix(file_path):
+    f = open(file_path, "r")
+    f.readline()
+    f.readline()
+    f.readline()
+    dimension = int(f.readline().split()[1])
+    print(dimension)
+    while f.readline().strip() != "EDGE_WEIGHT_SECTION":
+        continue
+    e = []
+    for i in range(dimension):
+        e.append([])
+        while len(e[-1]) < dimension:
+            e[-1].extend(map(int, f.readline().split()))
+
+    return e, dimension
