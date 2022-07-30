20a97c3c6123576e9c03a34adac1d2985a540662
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2022-07-30 13:40:12 +0300

    returning list in FGM

--- FGM.py
+++ FGM.py
@@ -4,37 +4,6 @@ from scipy import optimize
 from constants import INF
 
 
-# %%
-def make_matrices_from_file(file_path):
-    a = open(file_path)
-    a.readline()
-    a.readline()
-    a.readline()
-    dimension = int(a.readline().split()[1])
-    while a.readline().strip() != "EDGE_WEIGHT_SECTION":
-        continue
-    e = []
-    for p in range(dimension):
-        e.append([])
-        while len(e[-1]) < dimension:
-            tmp = map(int, a.readline().split())
-            for k in tmp:
-                if k == "100000000":
-                    k = "9999999"
-                e[-1].append(k)
-
-    return dimension, e
-
-
-# %%
-# MMAX = 10000
-# INF = 9999999
-# index_Edges = {}
-# n, M = make_matrices_from_file('ftv33.atsp')
-# m = n ** 2
-#
-
-# %%
 def minimum_Matching(graph, sz):
     row_ind, col_ind = optimize.linear_sum_assignment(graph)
     nodes = []
@@ -43,9 +12,9 @@ def minimum_Matching(graph, sz):
     x = 0
     for i in range(sz):
         t = i
-        if mark[t] == 0:
+        if (mark[t] == 0):
             x += 1
-        while not mark[t]:
+        while (not mark[t]):
             comp[t] = x - 1
             nodes.append(t)
             mark[t] = 1
@@ -56,53 +25,69 @@ def minimum_Matching(graph, sz):
 
 # %%
 def split_Graph(graph, sz):
-    new_graph = np.zeros((2 * sz, 2 * sz), dtype=int)
+    new_Graph = np.zeros((2 * sz, 2 * sz), dtype=int)
     for i in range(sz):
         for j in range(sz):
-            new_graph[i][j] = INF
+            new_Graph[i][j] = INF
     for i in range(sz):
         for j in range(sz):
-            new_graph[i][sz + j] = graph[i][j]
-    return new_graph
+            new_Graph[i][sz + j] = graph[i][j]
+    return new_Graph
 
 
 # %%
 def contract(graph, comp, sz):
-    new_size = 0
+    newSz = 0
     for i in range(sz):
-        new_size = max(new_size, comp[i])
-    new_size += 1
-    new_g = np.zeros((new_size, new_size), dtype=int)
-    for i in range(new_size):
-        new_g[i][i] = INF
-    in_edges = np.zeros(new_size, dtype=int)
-    out_edges = np.zeros(new_size, dtype=int)
+        newSz = max(newSz, comp[i])
+    newSz += 1
+    newG = np.zeros((newSz, newSz), dtype=int)
+    for i in range(newSz):
+        newG[i][i] = INF
+    in_Edges = np.zeros(newSz, dtype=int)
     for i in range(sz):
         for j in range(sz):
             if comp[i] != comp[j]:
-                new_g[comp[i]][comp[j]] = max(new_g[comp[i]][comp[j]], graph[i][j])
-    for i in range(sz):
-        for j in range(sz):
-            if comp[j] - comp[i] == 1:
-                if new_g[comp[i]][comp[j]] == graph[i][j]:
-                    out_edges[comp[i]] = i
-                    in_edges[comp[j]] = j
-            if (comp[i] == new_size - 1) and (comp[j] == 0):
-                out_edges[comp[i]] = i
-                in_edges[comp[j]] = j
-    added = 0
-    for i in range(new_size):
-        if in_edges[i] == out_edges[i]:
-            continue
-        added += graph[in_edges[i]][out_edges[i]]
-    return new_g, new_size, added
+                if (graph[i][j] > newG[comp[i]][comp[j]]):
+                    newG[comp[i]][comp[j]] = graph[i][j]
+                    in_Edges[comp[j]] = j
+    return newG, in_Edges, newSz
 
 
 # %%
 def solve(graph, sz):
+    tsp_List = []
     cost, nodes, comp = minimum_Matching(split_Graph(graph, sz), sz)
-    new_graph, new_size, added = contract(graph, comp, sz)
-    if new_size == 1:
-        return cost
-    return cost + added + solve(new_graph, new_size)
-
+    newGraph, in_Edges, newSz = contract(graph, comp, sz)
+    if (newSz == 1):
+        return nodes, cost
+    added = 0
+    mark = np.zeros(sz, dtype=int)
+    for i in range(newSz):
+        if i == newSz - 1:
+            added += graph[in_Edges[i]][in_Edges[0]]
+        else:
+            added += graph[in_Edges[i]][in_Edges[i + 1]]
+        x = in_Edges[i]
+        st = 0
+        en = 0
+        for j in range(sz):
+            if nodes[j] == x:
+                st = j
+                en = j
+        while (comp[nodes[st]] == i):
+            mark[nodes[st]] = 1
+            tsp_List.append(nodes[st])
+            st += 1
+            if (st == sz):
+                break
+        for j in range(sz):
+            if comp[nodes[j]] == i:
+                en = j
+                break
+        while (mark[nodes[en]] == 0):
+            mark[nodes[en]] = 1
+            tsp_List.append(nodes[en])
+            en += 1
+    new_List, newCost = solve(newGraph, newSz)
+    return tsp_List, cost + added + newCost
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 import FGM
 from asadpour import asadpour
 from utils import get_adjacency_matrix
-# f = open('nul', 'w')
-# sys.stderr = f
+f = open('nul', 'w')
+sys.stderr = f
 available_tests = ['br17.atsp', 'ft53.atsp', 'ft70.atsp', 'ftv170.atsp', 'ftv33.atsp', 'ftv35.atsp', 'ftv38.atsp',
                    'ftv44.atsp', 'ftv47.atsp', 'ftv55.atsp', 'ftv64.atsp', 'ftv70.atsp', 'kro124p.atsp', 'p43.atsp',
                    'rbg323.atsp', 'rbg358.atsp', 'rbg403.atsp', 'rbg443.atsp', 'ry48p.atsp']
@@ -24,7 +24,9 @@ while inp != "exit":
         M, n = get_adjacency_matrix("tests/" + available_tests[int(inp) - 1])
         print("Solution: " + str(solutions[available_tests[int(inp) - 1][:-5]]))
         print("FGM:")
-        print(FGM.solve(M, n))
+        vertex_list, answer = FGM.solve(M, n)
+        print(answer)
+        print(vertex_list)
         print("Held-Karp lower bound")
         try:
             print(asadpour(M, n)[0])
